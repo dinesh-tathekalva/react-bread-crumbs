d1b500450b4e86a976ba269c57f9e1f3c99c1cb7
dinesh-tathekalva <dinesh.tathekalva@gmail.com>
2025-10-23 20:52:18 -0500

    Added Breadcrumbs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Breadcrumbs from "./components/breadcrumbs";
+import Home from "./pages/home";
+import ProductListing from "./pages/product-listing";
+import ProductDetail from "./pages/product-detail";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="app">
+        <h1>RoadsideCoder Store</h1>
+        <Breadcrumbs />
+        <hr />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductListing />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
